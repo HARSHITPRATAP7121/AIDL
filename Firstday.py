316465d227c127b294612a6bd2538656d86b7c5f
HARSHITPRATAP7121 <2020pgicsharshit29@poornima.org>
2023-07-22 10:53:49 +0530

    commit

--- Firstday.py
+++ Firstday.py
@@ -11,3 +11,23 @@ git commit -m "commit"
 
 #Push the changes to Github 
 git push origin main
+
+#Brakets 
+{} - Dictoinary ,Set 
+[] - indexing/subscripting/slicing ,List
+()- Arguments , Expression,Tuple
+
+# Identifer 
+a= 1
+-It must contain alpa numeric only 
+- special char not allowed except  Underscore
+- not start from number 
+
+#Operator 
+1.Arthmetic: +, -, *,/ ,%(modulo), **(power),//(float div)
+2.assignment: =,+=,-=/...
+3.Relational: >,<,>=,<=,==,!=
+4.logical: and ,or,not
+5 Butwise: &,|,~,^(xor),>>(right),<<(left shift)
+6 membership: in ,not in 
+7 identity:   is ,is not
