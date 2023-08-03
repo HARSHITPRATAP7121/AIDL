e68c9a0988e57f95f405c71cc5d408498cf01cae
HARSHITPRATAP7121 <2020pgicsharshit29@poornima.org>
2023-08-03 10:16:33 +0530

    commit

--- Firstday.py
+++ Firstday.py
@@ -1,13 +1,13 @@
 #User congiguration
-  git config --global user.name "HARSHITPRATAP7121"
+  >git config --global user.name "HARSHITPRATAP7121"
   git config --global user.emal "2020pgicsharshit29@poornima.org"
 
 # Remote Repo connect
-git remote add origin " repo link "
+>git remote add origin " repo link "
 
 # Daily Process
-git add .
-git commit -m "commit"
+>git add .
+>git commit -m "commit"
 
 #Push the changes to Github 
 git push origin main
