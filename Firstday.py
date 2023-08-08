b9889d7578ef6016f9d5ca3d5ca0e76f279a1558
HARSHITPRATAP7121 <2020pgicsharshit29@poornima.org>
2023-08-08 09:25:55 +0530

    commit

---
+++ Firstday.py
@@ -0,0 +1,33 @@
+#User congiguration
+  >git config --global user.name "HARSHITPRATAP7121"
+  git config --global user.emal "2020pgicsharshit29@poornima.org"
+
+# Remote Repo connect
+>git remote add origin " repo link "
+
+# Daily Process
+>git add .
+>git commit -m "commit"
+
+#Push the changes to Github 
+git push origin main
+
+#Brakets 
+{} - Dictoinary ,Set 
+[] - indexing/subscripting/slicing ,List
+()- Arguments , Expression,Tuple
+
+# Identifer 
+a= 1
+-It must contain alpa numeric only 
+- special char not allowed except  Underscore
+- not start from number 
+
+#Operator 
+1.Arthmetic: +, -, *,/ ,%(modulo), **(power),//(float div)
+2.assignment: =,+=,-=/...
+3.Relational: >,<,>=,<=,==,!=
+4.logical: and ,or,not
+5 Butwise: &,|,~,^(xor),>>(right),<<(left shift)
+6 membership: in ,not in 
+7 identity:   is ,is not
